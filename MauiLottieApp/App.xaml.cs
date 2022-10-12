77232c85b83aef095036af8a16f50de2ee12bd11
Martin Mareček <martin.marecek@solitea.cz>
2022-10-12 10:04:59 +0200

    Add project files.

---
+++ MauiLottieApp/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace MauiLottieApp;
+
+public partial class App : Application
+{
+	public App(MainPage mainPage)
+	{
+		InitializeComponent();
+
+		//MainPage = new NavigationPage(new MainPage()); //Valid
+
+        MainPage = new NavigationPage(mainPage); //Invalid
+    }
+}
